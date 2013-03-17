ff55a7646086201ede17b10ab9a266013d28318f
Toni Tassani <atassani@gmail.com>
2013-03-17 17:28:55 +0100

    Replaced re-use of cryptSentence. Reformatted by Eclipse

--- encriptor/src/main/java/com/iexpertos/encriptor/Encriptor.java
+++ encriptor/src/main/java/com/iexpertos/encriptor/Encriptor.java
@@ -3,81 +3,60 @@ package com.iexpertos.encriptor;
 import java.security.InvalidParameterException;
 
 public class Encriptor {
-	
-	public String cryptWord(String word)
-	{
+
+	public String cryptWord(String word) {
 		validateInput(word);
-		
-		char[] wordArray = word.toCharArray();
-		String newWord = "";
-		for (int i = 0; i < word.length(); i++)
-		{
-			int charValue = wordArray[i];
-			newWord += String.valueOf((char)( charValue + 2));
-		}
-		
-		return newWord;
+		return cryptSentence(word);
 	}
-	
-	public String cryptWordToNumbers(String word)
-	{
+
+	public String cryptWordToNumbers(String word) {
 		validateInput(word);
-		
+
 		char[] wordArray = word.toCharArray();
 		String newWord = "";
-		for (int i = 0; i < word.length(); i++)
-		{
+		for (int i = 0; i < word.length(); i++) {
 			int charValue = wordArray[i];
 			newWord += String.valueOf(charValue + 2);
 		}
-		
+
 		return newWord;
 	}
 
-	public String cryptWord(String word, String charsToReplace)
-	{
+	public String cryptWord(String word, String charsToReplace) {
 		validateInput(word);
-		
+
 		char[] wordArray = word.toCharArray();
 		char[] replacement = charsToReplace.toCharArray();
 		char[] result = wordArray.clone();
-		for (int i = 0; i < wordArray.length; i++)
-		{
-			for (int j = 0; j < replacement.length; j++)
-			{
-				if (replacement[j] == wordArray[i])
-				{
+		for (int i = 0; i < wordArray.length; i++) {
+			for (int j = 0; j < replacement.length; j++) {
+				if (replacement[j] == wordArray[i]) {
 					int charValue = wordArray[i];
-					result[i] = (char)( charValue + 2);		
+					result[i] = (char) (charValue + 2);
 				}
 			}
 		}
 		return String.valueOf(result);
 	}
-	
-	public String cryptSentence(String sentence)
-	{
+
+	public String cryptSentence(String sentence) {
 		char[] sentenceArray = sentence.toCharArray();
 		String newWord = "";
-		for (int i = 0; i < sentence.length(); i++)
-		{
+		for (int i = 0; i < sentence.length(); i++) {
 			int charValue = sentenceArray[i];
-			newWord += String.valueOf((char)( charValue + 2));
+			newWord += String.valueOf((char) (charValue + 2));
 		}
-		
+
 		return newWord;
 	}
-	
-	public String[] getWords(String sentence)
-	{
+
+	public String[] getWords(String sentence) {
 		return sentence.split(" ");
 	}
-	
-	public void printWords(String sentence)
-	{
+
+	public void printWords(String sentence) {
 		String[] words = getWords(sentence);
-		for (String word : words)
-		{
+		for (String word : words) {
 			System.out.print("<" + word + ">");
 		}
 	}
