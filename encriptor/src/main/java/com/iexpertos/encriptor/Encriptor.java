e86de0683232f47d9e6bf6fc8a1ada6bbcde9a61
Toni Tassani <atassani@gmail.com>
2013-03-17 18:16:16 +0100

    Extracted new method on cryptWord to have all crypt methods with same structure.

--- encriptor/src/main/java/com/iexpertos/encriptor/Encriptor.java
+++ encriptor/src/main/java/com/iexpertos/encriptor/Encriptor.java
@@ -11,42 +11,31 @@ public class Encriptor {
 
 	public String cryptWordToNumbers(String word) {
 		validateInput(word);
-
 		StringBuilder newWord = new StringBuilder();
-		for (char charValue: word.toCharArray()) {
-			String newString = String.valueOf((int)charValue + 2);
+		for (char charValue : word.toCharArray()) {
+			String newString = String.valueOf((int) charValue + 2);
 			newWord.append(newString);
 		}
-
 		return newWord.toString();
 	}
 
 	public String cryptWord(String word, String charsToReplace) {
 		validateInput(word);
 		StringBuilder newWord = new StringBuilder();
-		for (char charValue: word.toCharArray()) {
-			String newString = null;
-			if (isCharInCharsToReplace(charValue, charsToReplace)){
-				newString = String.valueOf((char) (charValue + 2));
-			} else {
-				newString = String.valueOf(charValue);
-			}
-			newWord.append( newString );
+		for (char charValue : word.toCharArray()) {
+			String newString = transformCharIfInCharsToReplace(charValue,
+					charsToReplace);
+			newWord.append(newString);
 		}
 		return newWord.toString();
 	}
 
-	private boolean isCharInCharsToReplace(char theChar, String charsToReplace) {
-		return charsToReplace.indexOf(theChar) >= 0;
-	}
-
 	public String cryptSentence(String sentence) {
 		StringBuilder newWord = new StringBuilder();
-		for (char charValue: sentence.toCharArray()) {
-			String newString = String.valueOf((char) ((int)charValue + 2));
-			newWord.append( newString );
+		for (char charValue : sentence.toCharArray()) {
+			String newString = String.valueOf((char) ((int) charValue + 2));
+			newWord.append(newString);
 		}
-
 		return newWord.toString();
 	}
 
@@ -64,5 +53,20 @@ public class Encriptor {
 	private void validateInput(String word) {
 		if (word.contains(" "))
 			throw new InvalidParameterException();
+	}
+
+	private boolean isCharInCharsToReplace(char theChar, String charsToReplace) {
+		return charsToReplace.indexOf(theChar) >= 0;
+	}
+
+	private String transformCharIfInCharsToReplace(char charValue,
+			String charsToReplace) {
+		String newString;
+		if (isCharInCharsToReplace(charValue, charsToReplace)) {
+			newString = String.valueOf((char) (charValue + 2));
+		} else {
+			newString = String.valueOf(charValue);
+		}
+		return newString;
 	}
 }
